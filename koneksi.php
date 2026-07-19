0e70fd0a0e383390f3169ebb447c8915a53d67ae
Dzulkifli Hasbiallah <dzulkifli.hasby93@gmail.com>
2026-07-19 16:04:09 +0700

    feat:update database

--- koneksi.php
+++ koneksi.php
@@ -3,6 +3,6 @@
 // Timezone WIB — wajib ada agar jam tidak maju/mundur
 date_default_timezone_set('Asia/Jakarta');
 
-$koneksi = mysqli_connect("localhost", "root", "", "siswa");
+$koneksi = mysqli_connect("localhost", "root", "", "sid_bades");
 
 // Hanya jalankan kode ini JIKA tombol login sudah diklik
